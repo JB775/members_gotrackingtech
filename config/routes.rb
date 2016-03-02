0aef5fcd6338e7b79737c5dfdeded29934361f40
John Bergandino <jbergandino@gmail.com>
2016-03-02 14:41:09 -0500

    fixed carousel, added more content/pages and routes, backend in working order

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "content/gold"
   get "content/platinum"
   mount Payola::Engine => '/payola', as: :payola
-  root to: 'visitors#index'
+  root to: 'visitors#show'
   get 'products/:id', to: 'products#show', :as => :products
   devise_for :users, :controllers => { :registrations => 'registrations' }
   devise_scope :user do
